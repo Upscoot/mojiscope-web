10022bb7c102ddb12f9940c61f82759aac1f2cf5
Scott Adie <scottynosabe@gmail.com>
2022-06-16 22:44:52 -0400

    Mostly working

--- js/moji.js
+++ js/moji.js
@@ -1,27 +1,82 @@
 const partyBtn = document.getElementById("mojigo");
+const shareField = document.getElementById("share_field");
+const toastElement = document.getElementById('copyToast');
+const copyButton = document.getElementById("copyButton");
+const copyToast = new bootstrap.Toast(toastElement);
 let party = false;
 
+copyButton.addEventListener('click', () => {
+    copy();
+})
+
+function getRandom(arr, n) {
+    var result = new Array(n),
+        len = arr.length,
+        taken = new Array(len);
+    if (n > len)
+        throw new RangeError("getRandom: more elements taken than available");
+    while (n--) {
+        var x = Math.floor(Math.random() * len);
+        result[n] = arr[x in taken ? taken[x] : x];
+        taken[x] = --len in taken ? taken[len] : len;
+    }
+    return result;
+}
+
+const count = 50;
+const mojiList = getRandom(emojis, count);
+
 // TODO: Try rain again. Performance is bad on my phone...
+
+function copy() {
+    shareField.select()
+    document.execCommand("copy");
+    copyToast.show()
+}
 
 partyBtn.addEventListener("click", () => {
     const container = tsParticles.domItem(0);
 
     if (!party) {
-        console.log("Starting")
+        console.log("Starting");
         party = true;
+        partyBtn.classList.toggle("d-none");
         container.playEmitter(0);
         container.playEmitter(1);
 
+
+        setTimeout(function () {
+            console.log("Stopping")
+            container.pauseEmitter(0);
+            container.pauseEmitter(1);
+
+            const emoji1 = mojiList[1];
+            const emoji2 = mojiList[2];
+            const emoji3 = mojiList[3];
+            document.getElementById("moji_one").innerHTML = mojiList[1];
+            document.getElementById("moji_two").innerHTML = mojiList[2];
+            document.getElementById("moji_three").innerHTML = mojiList[3];
+            document.getElementById("results").classList.toggle("d-none");
+
+
+            shareField.innerHTML = "My Mojiscope for the day:\n" + emoji1 + " " + emoji2 + " " + emoji3 + "\nhttps://mojiscope.app"
+        }, 3000)
+
         //partyBtn.innerText = partyBtn.innerText.replace("Start", "Stop");
-    } else {
+    }/* else {
         console.log("Stopping")
         party = false;
 
         container.pauseEmitter(0);
         container.pauseEmitter(1);
 
+        document.getElementById("moji_one").innerHTML = mojiList[1];
+        document.getElementById("moji_two").innerHTML = mojiList[2];
+        document.getElementById("moji_three").innerHTML = mojiList[3];
+        document.getElementById("results").classList.toggle("d-none");
+
         //partyBtn.innerText = partyBtn.innerText.replace("Stop", "Start");
-    }
+    }*/
 });
 
 
@@ -42,46 +97,45 @@ tsParticles.load("tsparticles", {
         {
             autoPlay: false,
             position: {
-                x: 1,
-                y: 1
+                x: 50,
+                y: 95
             },
             rate: {
                 quantity: 5,
                 delay: 0.2
             },
-            direction: "bottom-right"
+            //direction: ["bottom", "]
         },
-        {
-            autoPlay: false,
-            position: {
-                x: 99,
-                y: 1
-            },
-            rate: {
-                quantity: 5,
-                delay: 0.2
-            },
-            direction: "bottom-left"
-        }
+        // {
+        //     autoPlay: false,
+        //     position: {
+        //         x: 99,
+        //         y: 1
+        //     },
+        //     rate: {
+        //         quantity: 5,
+        //         delay: 0.2
+        //     },
+        //     direction: "bottom-left"
+        // }
     ],
     particles: {
-        color: {
-            value: ["#1E00FF", "#FF0061", "#E1FF00", "#00FF9E"]
-        },
+        // color: {
+        //     value: ["#1E00FF", "#FF0061", "#E1FF00", "#00FF9E"]
+        // },
         move: {
-            decay: 0.01,
-            direction: "bottom",
+            decay: 0.0,
+            direction: "top",
             enable: true,
             gravity: {
-                enable: true,
+                enable: false,
                 acceleration: 9.81 * 4
             },
             outModes: {
-                bottom: "out",
                 default: "destroy"
             },
             size: true,
-            speed: { min: 10, max: 15 }
+            speed: {min: 10, max: 25}
         },
         number: {
             value: 0
@@ -113,7 +167,7 @@ tsParticles.load("tsparticles", {
         //     }
         //   },
         size: {
-            value: 32
+            value: 24
         },
         //   roll: {
         //     darken: {
@@ -143,7 +197,7 @@ tsParticles.load("tsparticles", {
                     {
                         fill: true,
                         font: "Verdana",
-                        value: emojis,
+                        value: mojiList,
                         style: "",
                         weight: 400
                     }
